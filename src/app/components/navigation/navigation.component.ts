4e3401fccf7451814c9415f0480e146721f66834
Manfred Huber <manfred.huber@untis.at>
2020-12-31 02:40:10 +0100

    adds navigation.component

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AuthenticationService } from '@service/authentication.service';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver,
+              private authenticationService: AuthenticationService) {}
+
+  logout(): void {
+    this.authenticationService.logout();
+  }
+}
