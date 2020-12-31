85dbaa77326287305052c5c3964bd29bd1c6182f
Manfred Huber <manfred.huber@untis.at>
2020-12-31 02:41:03 +0100

    use the new components in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '@service/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  isAuthenticated(): boolean {
+    return this.authenticationService.isAuthenticated();
+  }
 }
