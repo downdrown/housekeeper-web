4ca45a6111fda515585af0380cefb45726e485ae
Manfred Huber <manfred.huber@untis.at>
2021-01-01 02:23:13 +0100

    adds home.component to components.module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from '@app/components/login/login.component';
 import { MaterialModule } from '@app/material.module';
 import { NavigationComponent } from '@app/components/navigation/navigation.component';
@@ -14,10 +15,12 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   declarations: [
+    HomeComponent,
     LoginComponent,
     NavigationComponent
   ],
   exports: [
+    HomeComponent,
     LoginComponent,
     NavigationComponent
   ]
