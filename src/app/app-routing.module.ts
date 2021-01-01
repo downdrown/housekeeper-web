2dfff6ab3a15a6cfca556ef7aed2a45e066f64c9
Manfred Huber <manfred.huber@untis.at>
2021-01-01 02:04:07 +0100

    adds application routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '@app/components/login/login.component';
+import { NavigationComponent } from '@app/components/navigation/navigation.component';
+import { AuthenticationGuard } from '@app/guard/authentication.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: NavigationComponent, canActivate: [AuthenticationGuard], children:
+    [
+      // { path: 'child-a', component: ChildAComponent }
+    ]
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
