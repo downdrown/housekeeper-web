f051b41d2b38cb3b9c92f27a8a075c2d3680a01c
Manfred Huber <manfred.huber@untis.at>
2021-01-09 23:34:48 +0100

    sets up mobile and desktop navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -6,13 +6,27 @@ import { AuthenticationService } from '@service/authentication.service';
 import { UserService } from '@service/user.service';
 import { User } from '@app/models/user';
 import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-90deg)' })),
+      transition('rotated => default', animate('400ms ease-in')),
+      transition('default => rotated', animate('400ms ease-out'))
+    ])
+  ]
 })
 export class NavigationComponent implements OnInit {
+
+  @ViewChild(MatSidenav) sidenav: MatSidenav | undefined;
+
+  state = 'default';
 
   user: User | undefined;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -34,5 +48,10 @@ export class NavigationComponent implements OnInit {
   logout(): void {
     this.authenticationService.logout();
     this.router.navigateByUrl('login');
+  }
+
+  toggleSidenav(): void {
+    this.sidenav?.toggle();
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
   }
 }
