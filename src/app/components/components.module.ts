98491f1298f8a6bc54021f3c62e3559c7a9d332f
Manfred Huber <manfred.huber@untis.at>
2021-01-10 12:54:16 +0100

    refactors basic app navigation layout

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '@app/components/home/home.component';
 import { LoginComponent } from '@app/components/login/login.component';
 import { MaterialModule } from '@app/material.module';
 import { NavigationComponent } from '@app/components/navigation/navigation.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from '@app/components/profile/profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   declarations: [
     HomeComponent,
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -6,52 +6,41 @@ import { AuthenticationService } from '@service/authentication.service';
 import { UserService } from '@service/user.service';
 import { User } from '@app/models/user';
 import { Router } from '@angular/router';
-import { MatSidenav } from '@angular/material/sidenav';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css'],
-  animations: [
-    trigger('rotatedState', [
-      state('default', style({ transform: 'rotate(0)' })),
-      state('rotated', style({ transform: 'rotate(-90deg)' })),
-      transition('rotated => default', animate('400ms ease-in')),
-      transition('default => rotated', animate('400ms ease-out'))
-    ])
-  ]
+  styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
 
-  @ViewChild(MatSidenav) sidenav: MatSidenav | undefined;
-
-  state = 'default';
+  isMobile$: Observable<boolean> = this.observeBreakpoint([Breakpoints.Handset, Breakpoints.Tablet]);
+  isHandset$: Observable<boolean> = this.observeBreakpoint(Breakpoints.Handset);
+  isTablet$: Observable<boolean> = this.observeBreakpoint(Breakpoints.Tablet);
+  isWeb$: Observable<boolean> = this.observeBreakpoint(Breakpoints.Web);
 
   user: User | undefined;
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
 
   constructor(private authenticationService: AuthenticationService,
               private breakpointObserver: BreakpointObserver,
               private userService: UserService,
               private router: Router) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     const token = this.authenticationService.getToken();
     this.userService.readUser(token.username).then(user => this.user = user);
   }
 
-  logout(): void {
+  public logout(): void {
     this.authenticationService.logout();
     this.router.navigateByUrl('login');
   }
 
-  toggleSidenav(): void {
-    this.sidenav?.toggle();
-    this.state = (this.state === 'default' ? 'rotated' : 'default');
+  private observeBreakpoint(breakpoint: string | string[]): Observable<boolean> {
+    return this.breakpointObserver.observe(breakpoint)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
   }
 }
